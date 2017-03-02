6f9f7c7c1de9d76aafc9d36574a4f5f6df62a65e
jan-molak <jan.molak@gmail.com>
2017-03-02 21:26:26 +0000

    ci(book): The book is generated separately from the main build
    #29

--- scripts/travis/after_script.sh
+++ scripts/travis/after_script.sh
@@ -3,13 +3,13 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Pull requests do not affect the public Serenity/JS Handbook"
-  npm run book:build
+  npm run prebook:publish
   exit 0
 fi
 
 if [[ $TRAVIS_BRANCH != 'master' ]]; then
   echo "Building from a branch does not affect the public Serenity/JS Handbook"
-  npm run book:build
+  npm run prebook:publish
   exit 0
 fi
 
